15379657014f5b1df44b062494e0af19d09e1d4e
arundinesh1011@gmail.com <arundinesh1011@gmail.com>
2024-11-25 16:10:03 -0500

    Create new design for About page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './reusable/Header';  
 import Footer from './reusable/Footer';
+import About from './About/About'; 
 function App() {
   return (
     <div>
@@ -9,6 +11,12 @@ function App() {
         <div>
           <h1>Welcome to Ritual Website</h1>
         </div>
+        <Router>
+      <Routes>
+        {/* Add this Route */}
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
       </main>
       <Footer />
     </div>
