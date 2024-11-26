af50a93a009ecdee95b82f507ac6d9ff910be76f
arundinesh1011@gmail.com <arundinesh1011@gmail.com>
2024-11-25 21:07:28 -0500

    Linked about page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './Routes'; // Import the routing logic
 import Header from './reusable/Header'; // Common Header
 import Footer from './reusable/Footer'; // Common Footer
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './reusable/Header';  
-import Footer from './reusable/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './About/About'; 
 
 const App=() => {
@@ -17,12 +14,9 @@ const App=() => {
         <div>
           <h1>Welcome to Ritual Website</h1>
         </div>
-        <Router>
       <Routes>
-        {/* Add this Route */}
         <Route path="/about" element={<About />} />
       </Routes>
-    </Router>
       </main>
       <Footer />
     </Router>
